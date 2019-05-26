42ee42486dbbc157170ba2a560ffdf15bbbd0eb5
dnrsm <daikinarushima@gmail.com>
2019-05-27 02:09:51 +0900

    update Environment Variables

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+export default {
+  graphqlEndpoint: process.env.graphqlEndpoint,
+  region: process.env.region,
+  authenticationType: process.env.authenticationType,
+  apiKey: process.env.apiKey
+};
---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+export default {
+  Auth: {
+    identityPoolId: process.env.identityPoolId,
+    region: process.env.region,
+    userPoolId: process.env.userPoolId,
+    userPoolWebClientId: process.env.userPoolWebClientId,
+    mandatorySignIn: process.env.mandatorySignIn
+  }
+};
